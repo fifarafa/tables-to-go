63575abddee5b3d91270ccc1d1c1e10d7ad4c2d2
Filip Łubniewski <fifarafa@users.noreply.github.com>
2021-12-15 12:44:15 +0100

    Update tables-to-go-cli_test.go

--- internal/cli/tables-to-go-cli_test.go
+++ internal/cli/tables-to-go-cli_test.go
@@ -6,8 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
-	"github.com/fraenky8/tables-to-go/pkg/database"
-	"github.com/fraenky8/tables-to-go/pkg/settings"
+	"github.com/fifarafa/tables-to-go/pkg/database"
+	"github.com/fifarafa/tables-to-go/pkg/settings"
 )
 
 type mockDb struct {
