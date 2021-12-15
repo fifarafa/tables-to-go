1dcbbb38c496979192e25982faf272bf22ca528e
Filip Łubniewski <fifarafa@users.noreply.github.com>
2021-12-15 12:43:35 +0100

    Update tagger.go

--- pkg/tagger/tagger.go
+++ pkg/tagger/tagger.go
@@ -4,8 +4,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/fraenky8/tables-to-go/pkg/database"
-	"github.com/fraenky8/tables-to-go/pkg/settings"
+	"github.com/fifarafa/tables-to-go/pkg/database"
+	"github.com/fifarafa/tables-to-go/pkg/settings"
 )
 
 const (
